b5344e1b6ce5173eef6fc3c17ab4b94ae7f8488b
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-06 01:47:22 +0200

    Add files via upload

---
+++ pT-pre.tcl
@@ -0,0 +1,37 @@
+# pT-pre.tcl
+
+# Command handler for !pre
+bind pub - !pre preStats
+
+proc preStats {nick host handle channel text} {
+    global eggdrop
+
+    # Execute both shell scripts with the text after !pre as argument
+    set script_path1 "/glftpd/bin/pT-pre.sh"
+    set script_path2 "/glftpd/bin/pT-preapi.sh"
+
+    # Execute first script
+    set result1 [exec $script_path1 $text]
+
+    # Split the result into lines
+    set lines1 [split $result1 \n]
+
+    # Send each line as a message to the channel
+    foreach line1 $lines1 {
+        # Send the line as a message to the channel
+        putquick "PRIVMSG $channel :$line1"
+    }
+
+    # Execute second script
+    set result2 [exec $script_path2 $text]
+
+    # Split the result into lines
+    set lines2 [split $result2 \n]
+
+    # Send each line as a message to the channel
+    foreach line2 $lines2 {
+        # Send the line as a message to the channel
+        putquick "PRIVMSG $channel :$line2"
+    }
+}
+
---
+++ pT-pregrp.tcl
@@ -0,0 +1,36 @@
+# groupstats.tcl
+
+# Command handler for !group
+bind pub - !group groupSearch
+bind pub - !grp groupSearch
+
+proc groupSearch {nick host handle channel text} {
+    global eggdrop
+
+    # Announce that search is in progress
+    putquick "PRIVMSG $channel :Searching stats for $text - may take a moment"
+
+    # Execute the shell script with the text after !group as argument
+    set script_path "/glftpd/bin/pT-pregrp.sh"
+    set result [exec $script_path $text]
+
+    # Split the result into lines and send each line to the channel
+    foreach line [split $result \n] {
+        # Applying IRC colors for enhanced readability
+        set line [string map {
+            "Group has a total of" "Group has a total of"
+            "Last release:" "\00307Latest Release:\003"
+            "First release:" "\00307First Release:\003"
+            "Total size of releases:" "\00307Total size PRED:\003"
+            "Percentage of nuked releases:" "\00307Percentage of NUKED Releases:\003"
+            "NUKES:" "\00304NUKES:\003"
+            "Last NUKED release:" "\00304Latest NUKED Release:\003"
+            "UNNUKES:" "\00303UNNUKES:\003"
+            "Last UNNUKED release:" "\00303Latest UNNUKED Release:\003"
+        } $line]
+
+        # Send each formatted line to the channel
+        putquick "PRIVMSG $channel :$line"
+    }
+}
+
