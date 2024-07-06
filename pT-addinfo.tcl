82471302a9d3a11d693c67162dcbd365bf28d318
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-06 18:25:21 +0200

    Update pT-addinfo.tcl

--- pT-addinfo.tcl
+++ pT-addinfo.tcl
@@ -1,23 +1,19 @@
-# pT-addinfo.tcl
-
 # Command handler for !info
 bind pub - !info infoStats
 
-proc infoStats {nick host handle channel text} {
+proc addStats {nick host handle channel text} {
     global eggdrop
 
-    # Execute the shell script with the text after !info as arguments
+    # Execute the shell script with the text after !addpre as arguments
     set script_path "/eggdrop/scripts/sh/pT-addinfo.sh"
     set result [exec $script_path {*}[split $text]]
 
     # Split the result into lines
     set lines [split $result \n]
 
-    # Send each line as a private message to the user
+    # Announce each line to the specified channel
     foreach line $lines {
-        # Applying IRC colors for enhanced readability (modify as needed)
-        set colored_line "$line"
-
+        # Sending the line to the #destiny.site channel
+        putserv "PRIVMSG #somechan :$line"
     }
 }
-
