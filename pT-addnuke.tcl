54fc1fc2743299eb539f5a630a6f6c31b41d33a5
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-07 00:27:50 +0200

    Update pT-addnuke.tcl

--- pT-addnuke.tcl
+++ pT-addnuke.tcl
@@ -1,23 +1,19 @@
-# pT-addnuke.tcl
-
 # Command handler for !nuke
 bind pub - !nuke nukeStats
 
 proc nukeStats {nick host handle channel text} {
     global eggdrop
 
-    # Execute the shell script with the text after !nuke as arguments
-    set script_path "/eggdrop/scripts/sh/pT-addnuke.sh"
+    # Execute the shell script with the text after !addpre as arguments
+    set script_path "/glftpd/sitebot/scripts/sh/pT-addnuke.sh"
     set result [exec $script_path {*}[split $text]]
 
     # Split the result into lines
     set lines [split $result \n]
 
-    # Send each line as a private message to the user
+    # Announce each line to the specified channel
     foreach line $lines {
-        # Applying IRC colors for enhanced readability (modify as needed)
-        set colored_line "$line"
-
+        # Sending the line to the #destiny.site channel
+        putserv "PRIVMSG #somechan :$line"
     }
 }
-
