897e567f7e41b1fcd8012321238febb1ad6f7750
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-06 21:21:04 +0200

    Create pT-addold.tcl

---
+++ pT-addold.tcl
@@ -0,0 +1,19 @@
+# Command handler for !addpre
+bind pub - !addold oldStats
+
+proc oldStats {nick host handle channel text} {
+    global eggdrop
+
+    # Execute the shell script with the text after !addpre as arguments
+    set script_path "/eggdrop/scripts/sh/pT-addold.sh"
+    set result [exec $script_path {*}[split $text]]
+
+    # Split the result into lines
+    set lines [split $result \n]
+
+    # Announce each line to the specified channel
+    foreach line $lines {
+        # Sending the line to the #destiny.site channel
+        #putserv "PRIVMSG #somechan :$line"
+    }
+}
