00414192f1c21d69792f1627191ee094170964e7
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-07 00:28:11 +0200

    Update pT-addunnuke.tcl

--- pT-addunnuke.tcl
+++ pT-addunnuke.tcl
@@ -5,7 +5,7 @@ proc unnukeStats {nick host handle channel text} {
     global eggdrop
 
     # Execute the shell script with the text after !addpre as arguments
-    set script_path "/glftpd/sitebot/scripts/sh/pT-addunnuke.sh"
+    set script_path "/eggdrop/scripts/sh/pT-addunnuke.sh"
     set result [exec $script_path {*}[split $text]]
 
     # Split the result into lines
