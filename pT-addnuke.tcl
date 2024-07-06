028ea6bf14697111344e72c693d84fe40498a50f
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-07 00:45:36 +0200

    Update pT-addnuke.tcl

--- pT-addnuke.tcl
+++ pT-addnuke.tcl
@@ -5,7 +5,7 @@ proc nukeStats {nick host handle channel text} {
     global eggdrop
 
     # Execute the shell script with the text after !addpre as arguments
-    set script_path "/eggdrop/scripts/sh/pT-addnuke.sh"
+    set script_path "/glftpd/sitebot/scripts/sh/pT-addnuke.sh"
     set result [exec $script_path {*}[split $text]]
 
     # Split the result into lines
