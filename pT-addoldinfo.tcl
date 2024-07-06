37687829ecd6b75b19b54b3ce49ec9cf2f0d7a3b
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-06 21:21:35 +0200

    Update pT-addoldinfo.tcl

--- pT-addoldinfo.tcl
+++ pT-addoldinfo.tcl
@@ -5,7 +5,7 @@ proc oldinfoStats {nick host handle channel text} {
     global eggdrop
 
     # Execute the shell script with the text after !addpre as arguments
-    set script_path "/glftpd/sitebot/scripts/sh/pT-addoldinfo.sh"
+    set script_path "/eggdrop/scripts/sh/pT-addoldinfo.sh"
     set result [exec $script_path {*}[split $text]]
 
     # Split the result into lines
