1645e30d360f9e6eb6ac2fa6bdf50413ad120207
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-08 19:54:16 +0200

    Create pT-addsfv.tcl

---
+++ pT-addsfv.tcl
@@ -0,0 +1,19 @@
+# Command handler for !addsfv
+bind pub - !addsfv sfvStats
+
+proc sfvStats {nick host handle channel text} {
+    global eggdrop
+
+    # Execute the shell script with the text after !addpre as arguments
+    set script_path "/eggdrop/scripts/sh/pT-addsfv.sh"
+    set result [exec $script_path {*}[split $text]]
+
+    # Split the result into lines
+    set lines [split $result \n]
+
+    # Announce each line to the specified channel
+    foreach line $lines {
+        # Sending the line to the # channel
+        putserv "PRIVMSG $channel :$line"
+    }
+}
