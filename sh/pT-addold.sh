830ebe55945ddb63fb951a017b6dae123a69998a
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-08 18:09:48 +0200

    Update pT-addold.sh

--- sh/pT-addold.sh
+++ sh/pT-addold.sh
@@ -5,7 +5,7 @@ db_host=""
 db_user=""
 db_password=""
 db_name=""
-db_table="MAIN"  # Assuming MAIN is the main table
+main_table="MAIN"  # Assuming MAIN is the main table
 
 # Main script logic
 if [ $# -lt 4 ]; then
@@ -28,11 +28,11 @@ groupname=$(echo "$release" | awk -F '-' '{print $NF}')
 current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 
 # Check if release already exists in the database
-existing_release=$(mysql -h "$db_host" -u "$db_user" -p"$db_password" "$db_name" -N -B -e "SELECT rlsname FROM $db_table WHERE rlsname='$release';")
+existing_release=$(mysql -h "$db_host" -u "$db_user" -p"$db_password" "$db_name" -N -B -e "SELECT rlsname FROM $main_table WHERE rlsname='$release';")
 
 if [ -z "$existing_release" ]; then
     # Release does not exist, insert new record with status ADDOLD and section
-    query="INSERT INTO $db_table (rlsname, section, datetime, lastupdated, status, \`group\`) 
+    query="INSERT INTO $main_table (rlsname, section, datetime, lastupdated, status, \`group\`) 
            VALUES ('$release', '$section', '$datetime', '$current_datetime', 'ADDOLD', '$groupname');"
     # Execute MySQL query silently
     mysql -h "$db_host" -u "$db_user" -p"$db_password" "$db_name" -e "$query" > /dev/null 2>&1
@@ -41,7 +41,7 @@ if [ -z "$existing_release" ]; then
     echo "11[ADDOLD] ::7 $section :: $release"
 else
     # Release exists, update datetime, lastupdated, section, group, and status
-    query="UPDATE $db_table 
+    query="UPDATE $main_table 
            SET section = '$section',
                datetime = '$datetime',
                lastupdated = '$current_datetime',
