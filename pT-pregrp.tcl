5c15b1ec4c89a4d5c4fd573010423ea2e11133a7
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-08 08:19:53 +0200

    Update pT-pregrp.tcl

--- pT-pregrp.tcl
+++ pT-pregrp.tcl
@@ -1,36 +1,25 @@
 # pT-pregrp.tcl
 
-# Command handler for !group
-bind pub - !group groupSearch
-bind pub - !grp groupSearch
+# Command handler for !pre
+bind pub - !group grpStats
+bind pub - !grp grpStats
 
-proc groupSearch {nick host handle channel text} {
+proc grpStats {nick host handle channel text} {
     global eggdrop
 
-    # Announce that search is in progress
-    putquick "PRIVMSG $channel :Searching stats for $text - may take a moment"
+    # Execute both shell scripts with the text after !pre as argument
+    set script_path1 "/eggdrop/scripts/sh/pT-pregrp.sh"
 
-    # Execute the shell script with the text after !group as argument
-    set script_path "/eggdrop/scripts/sh/pT-pregrp.sh"
-    set result [exec $script_path $text]
+    # Execute first script
+    set result1 [exec $script_path1 $text]
 
-    # Split the result into lines and send each line to the channel
-    foreach line [split $result \n] {
-        # Applying IRC colors for enhanced readability
-        set line [string map {
-            "Group has a total of" "Group has a total of"
-            "Last release:" "\00307Latest Release:\003"
-            "First release:" "\00307First Release:\003"
-            "Total size of releases:" "\00307Total size PRED:\003"
-            "Percentage of nuked releases:" "\00307Percentage of NUKED Releases:\003"
-            "NUKES:" "\00304NUKES:\003"
-            "Last NUKED release:" "\00304Latest NUKED Release:\003"
-            "UNNUKES:" "\00303UNNUKES:\003"
-            "Last UNNUKED release:" "\00303Latest UNNUKED Release:\003"
-        } $line]
+    # Split the result into lines
+    set lines1 [split $result1 \n]
 
-        # Send each formatted line to the channel
-        putquick "PRIVMSG $channel :$line"
+    # Send each line as a message to the channel
+    foreach line1 $lines1 {
+        # Send the line as a message to the channel
+        putquick "PRIVMSG $channel :$line1"
     }
-}
 
+}
