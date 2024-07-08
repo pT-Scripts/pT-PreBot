6a3966da66fbe148dde1034c74f330dacdfa0f6f
Petabyte <74866068+pT-Scripts@users.noreply.github.com>
2024-07-08 08:19:16 +0200

    Update pT-predb.tcl

--- pT-predb.tcl
+++ pT-predb.tcl
@@ -1,13 +1,10 @@
-# pT-predb.tcl
+# db.tcl
 
 # Command handler for !db
 bind pub - !db dbStats
 
 proc dbStats {nick host handle channel text} {
     global eggdrop
-
-    # Announce that the DB info is being fetched
-    putquick "PRIVMSG $channel :Fetching DB info, Takes a while please wait."
 
     # Path to the shell script
     set script_path "/eggdrop/scripts/sh/pT-predb.sh"
@@ -30,4 +27,3 @@ proc dbStats {nick host handle channel text} {
         putquick "PRIVMSG $channel :Error executing script: $output"
     }
 }
-
